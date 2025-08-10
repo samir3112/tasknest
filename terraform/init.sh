477293ee3e1df010171787f2effcd010baf0d3b1
samir3112 <samirparate3112@gmail.com>
2025-08-10 07:45:02 +0000

    new

--- terraform/init.sh
+++ terraform/init.sh
@@ -7,3 +7,9 @@ usermod -aG docker ec2-user
 
 docker pull samir3112/tasknest-api
 docker run -d -p 80:5000 samir3112/tasknest-api
+
+# chmod 400 ~/tasknest/terraform/n-varginia.pem (Permission)
+# ssh -i n-varginia.pem ec2-user@<ec2-public-ip>
+# docker ps
+# docker ps -a (for all containers)
+# curl http://<ip-address-ec2>/tasks
