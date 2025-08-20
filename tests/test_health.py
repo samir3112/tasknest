0c0cc2a9afa9144d4f5c17756089608f614ee70a
samir3112 <samirparate3112@gmail.com>
2025-08-20 05:53:00 +0000

    SonarQube

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,12 +1,12 @@
 import pytest
-from app import app  # this refers to app.py
+from app import application   # import the exposed application
 
 @pytest.fixture
 def client():
-    with app.test_client() as client:
+    with application.test_client() as client:
         yield client
 
 def test_home_status(client):
     resp = client.get("/")
     assert resp.status_code == 200
-    assert b"Welcome to TaskNest ToDo API with RDS!" in resp.data
+    assert b"Welcome to TaskNest ToDo API" in resp.data
